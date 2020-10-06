b261245bddc19dddc4a3328a4701581cf3d82a7b
Burak <burakokumus98@gmail.com>
2020-10-06 11:23:58 +0300

    Create node class

---
+++ solution.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, left_missionary, right_missionary, left_cannibal, right_cannibal, boat_size, boat_location = 1, path = []):
+        self.left_missionary = left_missionary
+        self.right_missionary = right_missionary
+        # TODO
+
+    def generate_next(self):
+        return
+    
+    def __eq__(self, other):
+        return
+    
